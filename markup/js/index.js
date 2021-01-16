6f234858a18e0650e1b440b8c9691c2df6f5d1ff
xNone <65909678+xNone@users.noreply.github.com>
2021-01-16 14:13:09 +0200

    Update

--- markup/js/index.js
+++ markup/js/index.js
@@ -19,12 +19,10 @@ $('.slider').slick({
   slidesToScroll: 3,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1025,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true
+        slidesToScroll: 2
       }
     },
     {
